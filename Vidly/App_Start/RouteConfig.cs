49801f51c4d986a443fea6fd03bca664ce421b43
JoyChenTW <chrisjoy1864@hotmail.com>
2018-01-07 23:17:45 +0800

    Exercise2

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -26,7 +26,8 @@ namespace Vidly
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                //defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Customers", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
